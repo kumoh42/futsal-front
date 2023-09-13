e9ba649f5161c11676e1619679651d5c358cc1be
JeongHyeon2 <81457699+JeongHyeon2@users.noreply.github.com>
2023-09-13 17:00:59 +0900

    [DESIGN] const style 추가

---
+++ flutter_front/lib/common/const_styles/colors.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+const Color kMainColor = Color(0xFF344F65);
+const Color kSubColor = Color(0xFF4C4C4C);
+const Color kDisabledColor = Color(0xFFC6C6C6);
+const Color kPointColor = Color(0xFFEC255A);
+
+const Color kTextMainColor = Color(0xFF000000);
+const Color kTextSubColor = Color(0xFFC6C6C6);
+const Color kTextReverseColor = Color(0xFFFFFFFF);
+
+const Color kBackgroundMainColor = Color(0xFFFFFFFF);
+
+Color kBorderColor = const Color(0xFF000000).withOpacity(0.3);
---
+++ flutter_front/lib/common/const_styles/text_styles.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+import 'sizes.dart';
+
+const kTextMainStyleTitle = TextStyle(
+  color: kTextMainColor,
+  fontSize: kTextTitleSize,
+  fontWeight: FontWeight.bold,
+  overflow: TextOverflow.ellipsis,
+);
+
+const kTextMainStyleLarge = TextStyle(
+  color: kTextMainColor,
+  fontSize: kTextLargeSize,
+  fontWeight: FontWeight.bold,
+  overflow: TextOverflow.ellipsis,
+);
+
+const kTextMainStyleMiddle = TextStyle(
+  color: kTextMainColor,
+  fontSize: kTextMiddleSize,
+  fontWeight: FontWeight.bold,
+  overflow: TextOverflow.ellipsis,
+);
+const kTextMainStyleSmall = TextStyle(
+  color: kTextMainColor,
+  fontSize: kTextSmallSize,
+  fontWeight: FontWeight.bold,
+  overflow: TextOverflow.ellipsis,
+);
+
+const kTextDisabledStyleLarge = TextStyle(
+  color: kDisabledColor,
+  fontSize: kTextLargeSize,
+  fontWeight: FontWeight.bold,
+  overflow: TextOverflow.ellipsis,
+);
+
+const kTextDisabledStyleMiddle = TextStyle(
+  color: kDisabledColor,
+  fontSize: kTextMiddleSize,
+  fontWeight: FontWeight.bold,
+  overflow: TextOverflow.ellipsis,
+);
+
+const kTextReverseStyleLarge = TextStyle(
+  color: kTextReverseColor,
+  fontSize: kTextLargeSize,
+  fontWeight: FontWeight.bold,
+  overflow: TextOverflow.ellipsis,
+);
+
+const kTextReverseStyleMiddle = TextStyle(
+  color: kTextReverseColor,
+  fontSize: kTextMiddleSize,
+  fontWeight: FontWeight.bold,
+  overflow: TextOverflow.ellipsis,
+);
+const kTextReverseStyleMini = TextStyle(
+  color: kTextReverseColor,
+  fontSize: kTextMiniSize,
+  fontWeight: FontWeight.bold,
+  overflow: TextOverflow.ellipsis,
+);
+
+const kTextNormalStyleLarge = TextStyle(
+  color: kTextMainColor,
+  fontSize: kTextLargeSize,
+  fontWeight: FontWeight.normal,
+  overflow: TextOverflow.ellipsis,
+);
+
+const kTextNormalStyleMiddle = TextStyle(
+  color: kTextMainColor,
+  fontSize: kTextMiddleSize,
+  fontWeight: FontWeight.normal,
+  overflow: TextOverflow.ellipsis,
+);
+const kTextNormalStyleSmall = TextStyle(
+  color: kTextMainColor,
+  fontSize: kTextSmallSize,
+  fontWeight: FontWeight.normal,
+  overflow: TextOverflow.ellipsis,
+);
